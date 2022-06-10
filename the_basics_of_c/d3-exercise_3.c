524e3377155d62ea8e9c732c94143f61009e0ae4
Noah Binche <binchenoah@gmail.com>
2022-06-10 22:30:31 +0000

    Print numeric number

---
+++ the_basics_of_c/d3-exercise_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - print the  numeric value of (\n).
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	char character = '\n';
+
+	printf("The numeric value of \\n is: %d\n", character);
+
+	return (0);
+}
+  
