eda027cceffcf65eb5a6eba00a620c1e65ba42c3
Noah Binche <binchenoah@gmail.com>
2022-06-10 22:16:37 +0000

    Print floating number

---
+++ the_basics_of_c/d3-exercise_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - prints 123.456 in float
+  * 
+  * Return: 0
+  */
+
+int main(void)
+{
+	double dbl = 123.456;
+
+	printf("This is the floating point of 123.456: %f\n", dbl);
+
+	return (0);
+}
